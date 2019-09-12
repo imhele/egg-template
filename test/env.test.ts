a35ffb78563ba06f160966ff6582306b76a3bc4b
何乐 <work@imhele.com>
2019-09-12 11:01:31 +0800

    improve tests

--- test/env.test.ts
+++ test/env.test.ts
@@ -1,15 +1,23 @@
 import 'mocha';
 import 'tsconfig-paths/register';
 
+import extendApp from '@/extend/application';
 import { Env } from '@/utils/types';
 import assert from 'assert';
 import { app } from 'egg-mock/bootstrap';
 
 describe('test env', () => {
+  it('test extend.app.noop', () => {
+    assert.strictEqual(extendApp.noop(), undefined);
+    assert.strictEqual(extendApp.redis, null);
+    assert.strictEqual(extendApp.redis, null);
+  });
+
   it('is unit test', () => {
     assert.strictEqual(process.env.NODE_ENV, 'test');
     assert.strictEqual(process.env.EGG_SERVER_ENV, 'unittest');
   });
+
   it('test app.isEnv', () => {
     assert(app.isEnv(Env.Test));
     assert(!app.isEnv(Env.Dev));
